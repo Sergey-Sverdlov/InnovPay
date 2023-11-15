80341e661352ea62e8af0a3fec620a4847cc4250
Serega <61983829+Sergey-Sverdlov@users.noreply.github.com>
2023-11-15 17:01:45 +0300

    add component CardDeal

--- innovPay/src/components/Button.jsx
+++ innovPay/src/components/Button.jsx
@@ -3,7 +3,8 @@ import React from 'react';
 const Button = ({styles}) => {
     return (
         <div>
-            <button type="button" className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] text-primary outline-none ${styles}`}>Приступай к работе</button>
+            <button type="button" className={`py-4 px-6 bg-blue-gradient font-poppins 
+            font-medium text-[18px] text-primary outline-none ${styles} rounded-[10px]`}>Приступай к работе</button>
         </div>
     );
 };
--- innovPay/src/components/CardDeal.jsx
+++ innovPay/src/components/CardDeal.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
+import {card} from "../assets"
+import styles, {layout} from "../style";
+import Button from "./Button";
 
-const CardDeal = () => {
-    return (
-        <div>
-            
+const CardDeal = () => (
+    <section className={`${layout.section}`}>
+        <div className={layout.sectionInfo}>
+            <h2 className={`${styles.heading2} max-w-[470px] mt-5`}>Найдите более выгодное предложение по карте </h2>
+            <p className={`text-gradient`}>Arcu tortor, purus in mattis at sed integer faucibus.
+                Aliquet quis aliquet eget mauris tortor.ç Aliquet ultrices ac, ametau.</p>
+            <Button styles="mt-10 " />
         </div>
-    );
-};
+        <div className={layout.sectionImg}>
+            <img src={card} alt="card" className="w-[100%] h-[100%]" />
+        </div>
+
+    </section>
+);
 
 export default CardDeal;
